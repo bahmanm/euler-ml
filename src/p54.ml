d8125b62a917394777ee574f05fb5ff630073b84
Bahman Movaqar <Bahman@BahmanM.com>
2018-03-16 10:01:17 +0100

    solution to problem #54

--- src/p54.ml
+++ src/p54.ml
@@ -1,14 +1,6 @@
 (* Author: Bahman Movaqar <Bahman@BahmanM.com> *)
 open Batteries
 
-(** Card suits *)
-type suit_t =
-  | SC (** Clubs *)
-  | SH (** Hearts *)
-  | SD (** Diamonds *)
-  | SS (** Spades *)
-
-(*******************************************************************************)
 module Value : sig
   (** Card values *)
   type t = | V2 | V3 | V4 | V5 | V6 | V7 | V8 | V9
@@ -50,6 +42,9 @@ end = struct
 end
 
 (*******************************************************************************)
+(** Card suits *)
+type suit_t = | SC | SH | SD | SS
+
 (** Card *)
 type card_t = Card of Value.t * suit_t
 
@@ -97,89 +92,100 @@ end = struct
   let same_suit h =
     h |> suits |> List.unique |> List.length = 1
 
-  (* * * * * * * * * * * * * * * * * * *)
-  let highCard h =
-    Some(to_int HighCard, h |> sort_values)
+  (****************************************************************************)
+  module RankingFuns : sig
+    type t = (Value.t * suit_t) list -> (int * Value.t list) option
 
-  let onePair h =
-    match h |> sort_values |> List.group_consecutive (=) with
-    | [[p1; p2]; [v1]; [v2]; [v3]]
-    | [[v1]; [p1; p2]; [v2]; [v3]]
-    | [[v1]; [v2]; [p1; p2]; [v3]]
-    | [[v1]; [v2]; [v3]; [p1; p2]] ->
-      Some(to_int OnePair, [p1; v1; v2; v3])
-    | _ -> None
+    val funs: t list
+  end = struct
+    type t = (Value.t * suit_t) list -> (int * Value.t list) option
 
-  let twoPairs h =
-    match h |> sort_values |> List.group_consecutive (=) with
-    | [[p1; p2]; [p'1; p'2]; [v]]
-    | [[p1; p2]; [v]; [p'1; p'2]]
-    | [[v]; [p1; p2]; [p'1; p'2]] ->
-      Some(to_int TwoPairs, [p1; p'1; v])
-    | _ -> None
+    let highCard h =
+      Some(to_int HighCard, h |> sort_values)
 
-  let threeOfAKind h =
-    match h |> sort_values |> List.group_consecutive (=) with
-    | [[t1; t2; t3]; [v1]; [v2]]
-    | [[v1]; [t1; t2; t3]; [v2]]
-    | [[v1]; [v2]; [t1; t2; t3]] ->
-      Some(to_int ThreeOfAKind, [t1; v1; v2])
-    | _ -> None
+    let onePair h =
+      match h |> sort_values |> List.group_consecutive (=) with
+      | [[p1; p2]; [v1]; [v2]; [v3]]
+      | [[v1]; [p1; p2]; [v2]; [v3]]
+      | [[v1]; [v2]; [p1; p2]; [v3]]
+      | [[v1]; [v2]; [v3]; [p1; p2]] ->
+        Some(to_int OnePair, [p1; v1; v2; v3])
+      | _ -> None
 
-  let straight h =
-    match h |> sort_values with
+    let twoPairs h =
+      match h |> sort_values |> List.group_consecutive (=) with
+      | [[p1; p2]; [p'1; p'2]; [v]]
+      | [[p1; p2]; [v]; [p'1; p'2]]
+      | [[v]; [p1; p2]; [p'1; p'2]] ->
+        Some(to_int TwoPairs, [p1; p'1; v])
+      | _ -> None
+
+    let threeOfAKind h =
+      match h |> sort_values |> List.group_consecutive (=) with
+      | [[t1; t2; t3]; [v1]; [v2]]
+      | [[v1]; [t1; t2; t3]; [v2]]
+      | [[v1]; [v2]; [t1; t2; t3]] ->
+        Some(to_int ThreeOfAKind, [t1; v1; v2])
+      | _ -> None
+
+    let straight h =
+      match h |> sort_values with
       | [v1; v2; v3; v4; v5] when
           (Value.is_succ_of v1 v2) && (Value.is_succ_of v2 v3) &&
           (Value.is_succ_of v3 v4) && (Value.is_succ_of v4 v5) ->
         Some(to_int Straight, [v5])
       | _ -> None
 
-  let flush h =
-    if h |> same_suit |> not then
-      None
-    else
-      Some(to_int Flush, h |> sort_values)
+    let flush h =
+      if h |> same_suit |> not then
+        None
+      else
+        Some(to_int Flush, h |> sort_values)
 
-  let fullHouse h =
-    match h |> sort_values |> List.group_consecutive (=) with
-    | [[t1; t2; t3]; [p1; p2]]
-    | [[p1; p2]; [t1; t2; t3]] ->
-      Some(to_int FullHouse, [t1; p1])
-    | _ -> None
+    let fullHouse h =
+      match h |> sort_values |> List.group_consecutive (=) with
+      | [[t1; t2; t3]; [p1; p2]]
+      | [[p1; p2]; [t1; t2; t3]] ->
+        Some(to_int FullHouse, [t1; p1])
+      | _ -> None
 
-  let fourOfAKind h =
-    match h |> sort_values |> List.group_consecutive (=) with
-    | [[f1; f2; f3; f4]; [v]]
-    | [[v]; [f1; f2; f3; f4]] ->
-      Some(to_int FourOfAKind, [f1; v])
-    | _ -> None
+    let fourOfAKind h =
+      match h |> sort_values |> List.group_consecutive (=) with
+      | [[f1; f2; f3; f4]; [v]]
+      | [[v]; [f1; f2; f3; f4]] ->
+        Some(to_int FourOfAKind, [f1; v])
+      | _ -> None
 
-  let straigthFlush h =
-    if h |> same_suit |> not then
-      None
-    else
-      match straight h with
-      | Some(r, vs) -> Some(to_int StraightFlush, vs)
+    let straigthFlush h =
+      if h |> same_suit |> not then
+        None
+      else
+        match straight h with
+        | Some(r, vs) -> Some(to_int StraightFlush, vs)
+        | None -> None
+
+    let royalFlush h =
+      match (straigthFlush h) with
+      | Some(r, vs) ->
+        if Value.VA = (List.first vs) then
+          Some(to_int RoyalFlush, [])
+        else
+          None
       | None -> None
 
-  let royalFlush h =
-    match (straigthFlush h) with
-    | Some(r, vs) ->
-      if Value.VA = (List.first vs) then
-        Some(to_int RoyalFlush, [])
-      else
-        None
-    | None -> None
-
-  let compare (Hand h1) (Hand h2) =
-    let rank_funcs = [
+    let funs = [
       royalFlush; straigthFlush; fourOfAKind; fullHouse; flush;
       straight; threeOfAKind; twoPairs; onePair; highCard
-    ] in
-    let h1_cards = h1 |> List.map (fun (Card(v, s)) -> (v, s)) in
-    let h2_cards = h2 |> List.map (fun (Card(v, s)) -> (v, s)) in
-    let r1 = List.find_map (fun rf -> rf h1_cards) rank_funcs in
-    let r2 = List.find_map (fun rf -> rf h2_cards) rank_funcs in
+    ]
+  end
+
+  (****************************************************************************)
+  let compare (Hand h1) (Hand h2) =
+    let to_tuple (Card(v, s)) = (v, s) in
+    let h1_cards = h1 |> List.map to_tuple in
+    let h2_cards = h2 |> List.map to_tuple in
+    let r1 = List.find_map (fun rf -> rf h1_cards) RankingFuns.funs in
+    let r2 = List.find_map (fun rf -> rf h2_cards) RankingFuns.funs in
     Tuple2.compare
       ~cmp1:Int.compare
       ~cmp2:(List.compare Value.compare)
@@ -223,17 +229,17 @@ end = struct
     else
       let (ss1, ss2) = ss |> List.split_at 5 in
       let h1 = Hand(ss1 |> List.map card_of_string) in
-      let h2 = Hand(ss1 |> List.map card_of_string) in
+      let h2 = Hand(ss2 |> List.map card_of_string) in
       (h1, h2)
 end
 
 (*******************************************************************************)
-let solve hands =
+let solve' hands =
   hands
   |> Enum.map (fun s -> HandLoader.of_string s)
   |> Enum.map (fun (h1, h2) -> HandCompare.compare h1 h2)
   |> Enum.filter (fun i -> i = 1)
   |> Enum.count
 
-let solve_file path =
-  0
+let solve path =
+  File.lines_of path |> solve'
--- src/p54.mli
+++ src/p54.mli
@@ -2,8 +2,7 @@
 (* https://projecteuler.net/problem=54 *)
 open Batteries
 
-val solve: string Enum.t -> int
-
-val solve_file: string -> int
-(** [solve hands] solve the problem for [hands].
+val solve: string -> int
+(** [solve path] solve the problem. [path] is the path to a
+    file containing poker hands.
     https://projecteuler.net/problem=54 *)
