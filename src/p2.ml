b4a7b3238ddeb016c50a89ad8605dbda10b4c298
Bahman Movaqar <Bahman@BahmanM.com>
2018-05-14 11:16:56 +0200

    rewrite tail-recursively

--- src/p2.ml
+++ src/p2.ml
@@ -1,22 +1,15 @@
 (* Author: Bahman Movaqar <Bahman@BahmanM.com> *)
 open Batteries
 
-let fibo_enum max =
-  (fun () ->
-     let second_last_term = ref 0 in
-     let last_term = ref 1 in
-     (fun () ->
-        match !last_term + !second_last_term with
-        | n when n >= max -> raise Enum.No_more_elements
-        | n ->
-          second_last_term := !last_term;
-          last_term := n;
-          n
-     )
-  )() |> Enum.from
 
-let solve n =
-  fibo_enum n |>
-  Enum.fold
-    (fun acc e -> if e mod 2 = 0 then acc + e else acc)
-    0
+let solve limit =
+  let rec solve' term1 term2 result =
+    match term1 + term2 with
+    | term3 when term3 <= limit -> begin
+      match (term3 mod 2) with
+      | 0 -> solve' term2 term3 (result + term3)
+      | _ -> solve' term2 term3 result
+    end
+    | _ -> result
+  in
+  solve' 1 2 2
