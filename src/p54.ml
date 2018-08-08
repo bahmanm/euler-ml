7fa86f3a165c7250c84ac654d87571210465d22a
Bahman Movaqar <Bahman@BahmanM.com>
2018-08-08 14:33:40 +0200

    WIP rewrite the solution
    use monadic results

--- src/p54.ml
+++ src/p54.ml
@@ -1,5 +1,6 @@
 (* Author: Bahman Movaqar <Bahman@BahmanM.com> *)
 open Batteries
+open Result.Monad
 
 (******************************************************************************)
 module type HAND = sig
@@ -19,72 +20,76 @@ module Hand = struct
   type t = card_t * card_t * card_t * card_t * card_t
 
   let card_of_string s =
-    let clist = s |> String.to_list in
-    let suit = (List.at clist 1) |> function
-      | 'S' -> 1
-      | 'D' -> 2
-      | 'C' -> 3
-      | 'H' -> 4
-      | _ -> invalid_arg "card_of_string"
-    in
-    let value = (List.at clist 0) |> function
-      | c when c >= '2' && c <= '9' ->
-        (c |> Char.code) - ('0' |> Char.code)
-      | 'T' -> 10
-      | 'J' -> 11
-      | 'Q' -> 12
-      | 'K' -> 13
-      | 'A' -> 14
-      | _ -> invalid_arg "card_of_string"
-    in
-    (value, suit)
+    match s |> String.explode with
+    | [valuec; suitc] ->
+      begin match valuec with
+        | c when c >= '2' && c <= '9' ->
+          Ok((c |> Char.code) - ('0' |> Char.code))
+        | 'T' -> Ok(10)
+        | 'J' -> Ok(11)
+        | 'Q' -> Ok(12)
+        | 'K' -> Ok(13)
+        | 'A' -> Ok(14)
+        | _ -> Bad("invalid value")
+      end
+      >>= begin fun value ->
+        match suitc with
+        | 'S' -> Ok(value, 1)
+        | 'D' -> Ok(value, 2)
+        | 'C' -> Ok(value, 3)
+        | 'H' -> Ok(value, 4)
+        | _ -> Bad("invalid suit")
+      end
+    | _ -> Bad("invalid input")
 
   let of_string =
     (String.split_on_char ' ') %> function
-      | [c1; c2; c3; c4; c5] ->
-        (c1 |> card_of_string,
-         c2 |> card_of_string,
-         c3 |> card_of_string,
-         c4 |> card_of_string,
-         c5 |> card_of_string)
-      | _ -> invalid_arg "of_string"
+      | [c1s; c2s; c3s; c4s; c5s] ->
+        (c1s |> card_of_string)
+        >>= fun c1 -> c2s |> card_of_string
+        >>= fun c2 -> Ok(c1, c2)
+        >>= fun (c1, c2) -> c3s |> card_of_string
+        >>= fun c3 -> Ok(c1, c2, c3)
+        >>= fun (c1, c2, c3) -> c4s |> card_of_string
+        >>= fun c4 -> Ok(c1, c2, c3, c4)
+        >>= fun (c1, c2, c3, c4) -> c5s |> card_of_string
+        >>= fun c5 -> Ok(c1, c2, c3, c4, c5)
+      | _ -> Bad("invalid input")
 
   let pack_to_int = function
     | [v1; v2; v3; v4; v5] ->
-      (v5 lsl 16) + (v4 lsl 12) + (v3 lsl 8) + (v2 lsl 4) + v1
-    | _ -> invalid_arg "cards_to_int"
+      let n = (v5 lsl 16) + (v4 lsl 12) + (v3 lsl 8) + (v2 lsl 4) + v1 in
+      Ok(n)
+    | _ -> Bad("cards_to_int")
 
   let value ((v1, s1), (v2, s2), (v3, s3), (v4, s4), (v5, s5)) =
     let values = [v1; v2; v3; v4; v5] |> List.sort (-) in
-    let smallest = values |> List.first in
-    let nvalues = values |> List.map (fun v -> v - smallest) in
-    let gvalues = nvalues
-                  |> List.group Int.compare
-                  |> List.map List.length
-                  |> List.sort (-)
+    let normalised_values = values |> List.map (fun v -> v - (List.at values 0)) in
+    let grouped_values = normalised_values |> List.group Int.compare
+                         |> List.map List.length |> List.sort (-)
     in
     let rank =
       match s1 = s2 && s2 = s3 && s3 = s4 && s4 = s5 with
       | true -> begin
-          match nvalues with
+          match normalised_values with
           | [0; 1; 2; 3; 4] -> 0x800000 (* straight flush *)
           | _ -> 0x500000 (* flush *)
         end
       | false -> begin
-          match gvalues with
+          match grouped_values with
           | [1; 4] -> 0x700000 (* four of a kind *)
           | [2; 3] -> 0x600000 (* full house*)
           | [1; 1; 3] -> 0x300000 (* three of a kind *)
           | [1; 2; 2] -> 0x200000 (* two pairs *)
           | [1; 1; 1; 2] -> 0x100000 (* one pair *)
           | [1; 1; 1; 1; 1] -> begin
-              match nvalues with
+              match normalised_values with
               | [0; 1; 2; 3; 4] -> 0x400000 (* straight *)
               | _ -> 0x00000
             end
           | _ -> invalid_arg "value"
         end
     in
-    rank + (values |> pack_to_int)
+    (values |> pack_to_int) >>= fun value -> Ok(value + rank)
 
 end
--- test/test_p54.ml
+++ test/test_p54.ml
@@ -1,94 +1,91 @@
 (* Author: Bahman Movaqar <Bahman@BahmanM.com> *)
 open OUnit2
 open Batteries
-open Printf
 
 module HandSuite = struct
   let card_of_string _ =
     let data = "2S" in
-    let expected = (2, 1) in
+    let expected = Ok(2, 1) in
     let actual = data |> P54.Hand.card_of_string in
     assert_equal expected actual;
 
     let data = "TC" in
-    let expected = (10, 3) in
+    let expected = Ok(10, 3) in
     let actual = data |> P54.Hand.card_of_string in
     assert_equal expected actual;
 
     let data = "AH" in
-    let expected = (14, 4) in
+    let expected = Ok(14, 4) in
     let actual = data |> P54.Hand.card_of_string in
     assert_equal expected actual
 
   let of_string _ =
     let data = "8C TS KC 9H 4S" in
-    let expected = ((8, 3), (10, 1), (13, 3), (9, 4), (4, 1)) in
+    let expected = Ok((8, 3), (10, 1), (13, 3), (9, 4), (4, 1)) in
     let actual = data |> P54.Hand.of_string in
     assert_equal expected actual;
 
     let data = "TS AS KC QH JS" in
-    let expected = ((10, 1), (14, 1), (13, 3), (12, 4), (11, 1)) in
+    let expected = Ok((10, 1), (14, 1), (13, 3), (12, 4), (11, 1)) in
     let actual = data |> P54.Hand.of_string in
     assert_equal expected actual
 
   let pack_to_int _ =
     let data = [1; 2; 3; 4; 5] in
-    let expected = 0x54321 in
+    let expected = Ok(0x54321) in
     let actual = data |> P54.Hand.pack_to_int in
     assert_equal expected actual;
 
     let data = [4; 8; 9; 0xc; 0xc] in
-    let expected = 0xcc984 in
+    let expected = Ok(0xcc984) in
     let actual = data |> P54.Hand.pack_to_int in
     assert_equal expected actual
 
   let value _ =
-    let hexprinter = sprintf "%x" in
-
     let data = ((2, 1), (2, 1), (3, 1), (4, 1), (7, 1)) in
-    let expected = 0x574322 in
+    let expected = Ok(0x574322) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"flush" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"flush" expected actual;
 
     let data = ((2, 1), (3, 1), (4, 1), (5, 1), (6, 1)) in
-    let expected = 0x865432 in
+    let expected = Ok(0x865432) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"straigh flush" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"straigh flush" expected actual;
 
     let data = ((2, 1), (3, 2), (4, 4), (5, 1), (6, 3)) in
-    let expected = 0x465432 in
+    let expected = Ok(0x465432) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"straight" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"straight" expected actual;
 
     let data = ((2, 1), (2, 2), (2, 3), (2, 4), (7, 1)) in
-    let expected = 0x772222 in
+    let expected = Ok(0x772222) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"four of a kind" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"four of a kind" expected actual;
 
     let data = ((2, 1), (2, 2), (2, 4), (4, 1), (4, 4)) in
-    let expected = 0x644222 in
+    let expected = Ok(0x644222) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"full house" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"full house" expected actual;
 
     let data = ((2, 1), (2, 2), (2, 4), (4, 1), (6, 3)) in
-    let expected = 0x364222 in
+    let expected = Ok(0x364222) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"three of a kind" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"three of a kind" expected actual;
 
     let data = ((2, 1), (2, 2), (4, 4), (4, 1), (6, 3)) in
-    let expected = 0x264422 in
+    let expected = Ok(0x264422) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"two pairs" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"two pairs" expected actual;
 
     let data = ((2, 1), (2, 2), (4, 4), (9, 1), (11, 3)) in
-    let expected = 0x1b9422 in
+    let expected = Ok(0x1b9422) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"one pair" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"one pair" expected actual;
 
     let data = ((2, 1), (3, 2), (6, 4), (8, 1), (11, 3)) in
-    let expected = 0x0b8632 in
+    let expected = Ok(0x0b8632) in
     let actual = data |> P54.Hand.value in
-    assert_equal ~msg:"high card" ~printer:hexprinter expected actual;
+    assert_equal ~msg:"high card" expected actual;
 end
 
 let p54_suite =
