601efb7159f22e62b64fd98b999b71d0574ac165
Bahman Movaqar <Bahman@BahmanM.com>
2018-08-10 18:17:01 +0200

    remove mli for better testability

--- src/p2.mli
+++
@@ -1,6 +0,0 @@
-(* Author: Bahman Movaqar <Bahman@BahmanM.com> *)
-(* https://projecteuler.net/problem=2 *)
-
-val solve: int -> int
-(** [solve n]
-    https://projecteuler.net/problem=2 where [n=4,000,000] *)
